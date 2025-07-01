f3be8f54d9131b8d17a977954c13c2141a8a58b2
bhavesh <bhmahajan055@gmail.com>
2025-07-01 23:20:38 +0530

    final committ

--- lms/src/main/java/org/bhavesh/lms/repository/Inventory.java
+++ lms/src/main/java/org/bhavesh/lms/repository/Inventory.java
@@ -3,13 +3,19 @@ package org.bhavesh.lms.repository;
 import org.bhavesh.lms.model.Book;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Inventory {
     private List<Book> books = new ArrayList<>();
+    private Map<String, Book> isbnMap = new HashMap<>();
 
     public boolean addBook(Book book) {
-        return books.add(book);
+        if (isbnMap.containsKey(book.getIsbn())) return false;
+        books.add(book);
+        isbnMap.put(book.getIsbn(), book);
+        return true;
     }
 
     public boolean removeBookByIsbn(String isbn) {
@@ -21,8 +27,8 @@ public class Inventory {
     }
 
     public Book findAvailableBookByIsbn(String isbn) {
-        return books.stream()
-                .filter(book -> book.getIsbn().equals(isbn) && !book.isBorrowed())
-                .findFirst().orElse(null);
+        if (isbnMap.containsKey(isbn))
+            return isbnMap.get(isbn);
+        return null;
     }
 }
